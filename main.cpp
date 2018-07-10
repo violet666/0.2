d88b9742336e6a92b2a922fbddc0ea74632f3f18
violet666 <2336942795@qq.com>
2018-07-10 18:02:15 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    for(int i = 1; ;i++){
+        int x = i*i;
+        if(x < 1000) continue;
+        if(x > 9999) break;
+        int fro = x / 100;
+        int bac = x % 100;
+        if(fro/10 == fro%10 && bac/10 == bac%10)
+            printf("%d\n",x);
+    }
+    return 0;
+}
